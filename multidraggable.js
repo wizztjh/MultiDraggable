3336d72fc23a4dcbfbe80ea362161b8fab1dbbb7
wizztjh <wizztjh@wizztjh-Presario-CQ42-Notebook-PC.(none)>
2012-03-23 12:13:53 +0800

    to solve the problem of changing set of dom object that have the class name.
    so by parsing in a jquery selector string will select the dom object when it start to drag.
    
    Signed-off-by: wizztjh <wizztjh@wizztjh-Presario-CQ42-Notebook-PC.(none)>

--- multidraggable.js
+++ multidraggable.js
@@ -18,27 +18,44 @@
 		            $(this).data("init", true).draggable(opts,{
 				         start: function (event,ui) {
 							var pos = $(this).position();
-							$.each(opts.group || {}, function(key,value) {
-								var elemPos = $(value).position();
-								initLeftOffset[key] = elemPos.left - pos.left;
-								initTopOffset[key] = elemPos.top - pos.top;			
-							});		
+							
+							if (typeof opts.group == "string"){
+								opts.group = $(opts.group);
+							}
+							
+							opts.group.not($(this));
+							
+							if(opts.group.length>0){
+								$.each(opts.group || {}, function(key,value) {
+									var elemPos = $(value).position();
+									initLeftOffset[key] = elemPos.left - pos.left;
+									initTopOffset[key] = elemPos.top - pos.top;			
+								});
+							}
 							opts.startNative ? opts.startNative() : {};
 						},
 						drag: function(event,ui) {
 							var pos = $(this).offset();
-							$.each(opts.group || {}, function(key,value) {
-								$(value).offset({left: pos.left + initLeftOffset[key], 
-								top: pos.top + initTopOffset[key]});				
-							});
+							
+							if(opts.group.length>0){
+								$.each(opts.group || {}, function(key,value) {
+									$(value).offset({left: pos.left + initLeftOffset[key], 
+									top: pos.top + initTopOffset[key]});				
+								});
+							}
+							
 							opts.dragNative ? opts.dragNative() : {};
 						},
 						stop: function(event,ui) {
 							var pos = $(this).offset();
-							$.each(opts.group || {}, function(key,value) {
-								$(value).offset({left: pos.left + initLeftOffset[key], 
-								top: pos.top + initTopOffset[key]});								
-							});
+							
+							if(opts.group.length>0){
+								$.each(opts.group || {}, function(key,value) {
+									$(value).offset({left: pos.left + initLeftOffset[key], 
+									top: pos.top + initTopOffset[key]});								
+								});
+							}
+							
 							opts.stopNative ? opts.stopNative() : {};
 						},
 					});
